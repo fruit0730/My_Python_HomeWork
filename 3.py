2060c67c47884f51f26d5ed945e2bf044316969c
果的代码 <13694111+pipa3288582557@user.noreply.gitee.com>
2025-12-20 13:05:29 +0800

    交作业

--- 3.py
+++ 3.py
@@ -26,5 +26,25 @@ def 字符串练习(strs):
 
     strs3=strs.lower()
     print("lower方法的作用\n",strs3)
-浮点数练习题()
-字符串练习("ss")
+
+def 其他常用字符串处理方法方法():
+    s="How are you doing?"
+    d="This is a IPhone for AppleStore"
+
+    print("split方法的作用\n",s.split())         #分隔字符
+
+    print("count方法的作用\n",s.count("o"))      #统计字符出现的次数
+
+    print("replace方法的作用\n",d.replace("IPhone","OPPO Phone"))      #替换字符 new若留空则为删除
+
+
+
+def 方法():
+    e = "How are you doing?"
+    f = "This is a IPhone for AppleStore"
+    print(e.join(f))
+
+#浮点数练习题()
+#字符串练习(input("请输入文本\n"))
+#其他常用字符串处理方法方法()
+方法()
